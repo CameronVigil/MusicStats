7f63293b61df73224b3b881b35cb8fd8b7165899
splitscrn <cameronvigil4@gmail.com>
2025-10-08 20:58:44 -0500

    Initial Commit
    Initial Commit

---
+++ apple-music-events/src/api/apple-token.js
@@ -0,0 +1,20 @@
+﻿import fs from "fs";
+import jwt from "jsonwebtoken";
+
+export default function handler(req, res) {
+    const teamId = "PC84YF525S";
+    const keyId = "9B83MQYGSJ";
+    const privateKey = fs.readFileSync("./AuthKey_X324HMKBA9.p8").toString();
+
+    const token = jwt.sign({}, privateKey, {
+        algorithm: "ES256",
+        expiresIn: "180d",
+        issuer: teamId,
+        header: {
+            alg: "ES256",
+            kid: keyId,
+        },
+    });
+
+    res.status(200).json({ token });
+}
