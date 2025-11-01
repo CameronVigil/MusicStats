dc584791f3ccf7be800d2e138626add8f18d9e88
splitscrn <cameronvigil4@gmail.com>
2025-10-31 21:23:19 -0500

    Remove unused code
    Remove unused code

--- apple-music-events/api/music-summaries.jsx
+++ apple-music-events/api/music-summaries.jsx
@@ -33,30 +33,6 @@ export default async function handler(req, res) {
         console.log("Retrieved summaries.");
         const data = await appleRes.data;
 
-        const appleRes2 = await fetch(
-            `https://amp-api.music.apple.com/v1/me/music-summaries/year-2025?l=en-us&omit[resource]=autos&extend=editorialVideo,listeningStreaks&views=top-artists,top-albums,top-songs&fields[songs]=name,artistName,artwork,previews,editorialVideo,genreNames,contentRating,url&fields[artists]=name,artwork,editorialVideo,url&fields[albums]=name,artwork,artistName,editorialVideo,contentRating,editorialVideo,url&fields[stations]=name,artwork,url&fields[playlists]=name,artwork,url,editorialVideo&include[music-summaries]=playlist&include[artist-period-summaries]=artist&include[album-period-summaries]=album&include[song-period-summaries]=song`,
-            {
-                headers: {
-                    Authorization: `Bearer ${developerToken}`,
-                    "Music-User-Token": userToken,
-                },
-            }
-        );
-        console.log("Retrieved summaries.");
-        const data2 = await appleRes2.data;
-
-        const appleRes3 = await fetch(
-            `https://amp-api.music.apple.com/v1/me/music-summaries/month-2025-9?extend=insightKinds,listeningStreaks&views=top-artists,top-albums,top-songs-for-months,top-playlists,top-genres,top-stations&fields[artists]=name,artwork,editorialVideo&fields[albums]=name,artwork,artistName,contentRating,editorialVideo&fields[stations]=name,artwork&fields[playlists]=name,artwork,url,editorialVideo&fields[songs]=name,artistName,artwork,previews,genreNames,contentRating&include[playlist-period-summaries]=playlist&include[station-period-summaries]=station&include[genre-period-summaries]=genres&include[monthly-song-period-summaries]=song`,
-            {
-                headers: {
-                    Authorization: `Bearer ${developerToken}`,
-                    "Music-User-Token": userToken,
-                },
-            }
-        );
-        console.log("Retrieved summaries.");
-        const data3 = await appleRes3.data;
-
         // Send CORS headers for the actual response
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
@@ -68,9 +44,3 @@ export default async function handler(req, res) {
         res.status(500).json({ error: err.message });
     }
 }
-/*	https://amp-api.music.apple.com/v1/me/music-summaries/search?l=en-us&omit[resource]=autos&period=month&year=2025&include=top-songs-abridged,top-albums-abridged,top-artists-abridged&fields[songs]=name,artwork&fields[artists]=name,artwork&fields[albums]=name,artwork
-
-https://amp-api.music.apple.com/v1/me/music-summaries/year-2025?l=en-us&omit[resource]=autos&extend=editorialVideo,listeningStreaks&views=top-artists,top-albums,top-songs&fields[songs]=name,artistName,artwork,previews,editorialVideo,genreNames,contentRating,url&fields[artists]=name,artwork,editorialVideo,url&fields[albums]=name,artwork,artistName,editorialVideo,contentRating,editorialVideo,url&fields[stations]=name,artwork,url&fields[playlists]=name,artwork,url,editorialVideo&include[music-summaries]=playlist&include[artist-period-summaries]=artist&include[album-period-summaries]=album&include[song-period-summaries]=song
-https://amp-api.music.apple.com/v1/me/music-summaries/month-2025-9?extend=insightKinds,listeningStreaks&views=top-artists,top-albums,top-songs-for-months,top-playlists,top-genres,top-stations&fields[artists]=name,artwork,editorialVideo&fields[albums]=name,artwork,artistName,contentRating,editorialVideo&fields[stations]=name,artwork&fields[playlists]=name,artwork,url,editorialVideo&fields[songs]=name,artistName,artwork,previews,genreNames,contentRating&include[playlist-period-summaries]=playlist&include[station-period-summaries]=station&include[genre-period-summaries]=genres&include[monthly-song-period-summaries]=song
-
-*/
